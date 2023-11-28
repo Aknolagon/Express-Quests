a3eb0ab99968c1639e64292fb6ab8f2d53850e78
Aknolagon <kalki.prasanna.co@gmail.com>
2023-11-28 22:10:36 +0100

    quête 2.1

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -1,32 +1,5 @@
 const database = require("../../database");
 
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    color: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    color: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
 const getMovies = (req, res) => {
   database
     .query("select * from movies")
@@ -34,7 +7,6 @@ const getMovies = (req, res) => {
       res.json(movies); // use res.json instead of console.log
     })
     .catch((err) => {
-      console.error(err);
       res.sendStatus(500);
     });
 };
@@ -52,37 +24,6 @@ const getMovieById = (req, res) => {
       }
     })
     .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const getUsers = (req, res) => {
-  database
-    .query("select * from users")
-    .then(([users]) => {
-      res.json(users); // use res.json instead of console.log
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const getUsersById = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  database
-    .query("select * from users where id = ?", [id])
-    .then(([users]) => {
-      if (users[0] != null) {
-        res.json(users[0]);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
       res.sendStatus(500);
     });
 };
@@ -99,24 +40,6 @@ const postMovie = (req, res) => {
       res.status(201).send({ id: result.insertId });
     })
     .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const postUser = (req, res) => {
-  const { firstname, lastname, email, city, language } = req.body;
-
-  database
-    .query(
-      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-      [firstname, lastname, email, city, language]
-    )
-    .then(([result]) => {
-      res.status(201).send({ id: result.insertId });
-    })
-    .catch((err) => {
-      console.error(err);
       res.sendStatus(500);
     });
 };
@@ -124,8 +47,5 @@ const postUser = (req, res) => {
 module.exports = {
   getMovies,
   getMovieById,
-  getUsers,
-  getUsersById,
   postMovie,
-  postUser,
 };
--- tests/users.test.js
+++ tests/users.test.js
@@ -6,6 +6,8 @@ const database = require("../database");
 
 afterAll(() => database.end());
 
+const crypto = require("node:crypto");
+
 describe("GET /api/users", () => {
   it("should return all users", async () => {
     const response = await request(app).get("/api/users");
@@ -32,3 +34,44 @@ describe("GET /api/users/:id", () => {
   });
 });
 
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Marie",
+      lastname: "Martin",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Paris",
+      language: "French",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [usersDatabase] = result;
+
+    expect(usersDatabase).toHaveProperty("id");
+    expect(usersDatabase).toHaveProperty("email");
+    expect(usersDatabase.firstname).toStrictEqual(newUser.firstname);
+    expect(usersDatabase.lastname).toStrictEqual(newUser.lastname);
+    expect(usersDatabase.email).toStrictEqual(newUser.email);
+    expect(usersDatabase.city).toStrictEqual(newUser.city);
+    expect(usersDatabase.language).toStrictEqual(newUser.language);
+  });
+  it("should return an error", async () => {
+    const userWithMissingProps = { email : "harry.potter@wild.co" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
+  });
+});
