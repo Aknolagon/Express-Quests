3aa6672d2cb7d79b0b30bc08d2ff2a879878c11d
Aknolagon <kalki.prasanna.co@gmail.com>
2023-11-29 23:29:37 +0100

    quest 2.0

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -57,36 +57,6 @@ const getMovieById = (req, res) => {
     });
 };
 
-const getUsers = (req, res) => {
-  database
-    .query("select * from users")
-    .then(([users]) => {
-      res.json(users); // use res.json instead of console.log
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
-const getUsersById = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  database
-    .query("select * from users where id = ?", [id])
-    .then(([users]) => {
-      if (users[0] != null) {
-        res.json(users[0]);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
 const postMovie = (req, res) => {
   const { title, director, year, color, duration } = req.body;
 
@@ -104,28 +74,8 @@ const postMovie = (req, res) => {
     });
 };
 
-const postUser = (req, res) => {
-  const { firstname, lastname, email, city, language } = req.body;
-
-  database
-    .query(
-      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-      [firstname, lastname, email, city, language]
-    )
-    .then(([result]) => {
-      res.status(201).send({ id: result.insertId });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
 module.exports = {
   getMovies,
   getMovieById,
-  getUsers,
-  getUsersById,
   postMovie,
-  postUser,
 };
